b445088e86e3a1f339dd61c472be1b74cfa783a1
ppilla1 <informprashant@gmail.com>
2019-12-24 10:25:42 +0530

    Initial commit

---
+++ src/main/java/org/example/geode/SpringBootGeodeExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.geode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGeodeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGeodeExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/geode/SpringBootGeodeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.geode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGeodeExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
